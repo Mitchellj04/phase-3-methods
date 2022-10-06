b65d071e11fc185964172527e57c25ccee23060d
JustinMitchell <mitchelljm04@gmail.com>
2022-10-06 11:49:27 -0400

    commit tree 3e32a24644f6c4e7e0d997594b6c7e425f4312c0

--- methods.rb
+++ methods.rb
@@ -1,1 +1,27 @@
 # Your code here!
+
+
+#greet_programmer
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+#greet
+def greet (name)
+    puts "Hello, #{name}!"
+end
+
+#greet_with_default
+def greet_with_default (name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+#add
+def add(num,num2)
+    num+num2
+end
+
+#halve 
+def halve(num1)
+   return num1/2
+end
